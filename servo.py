09359df7d1610b267b688992fd61759d498ab5a4
Sahil Udayasingh <48101923+Sahil1216@users.noreply.github.com>
2019-07-31 07:03:47 +0530

    Controlling Servo code

---
+++ servo.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(12, GPIO.OUT)
+
+p=GPIO.PWM(12,50)
+
+p.start(7.5)
+
+try:
+    while True:
+        p.ChangeDutyCycle(7.5)
+        time.sleep(1)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(1)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(1)
+except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
